837ca08e2c2ae5224382ac5781e8625896cd3aff
Aliaksei Kavaliou <aliaksei.kavaliou@geniussports.com>
2019-08-08 19:39:48 +0300

    makeBetHandler tess append

--- tests/unit/Handler/MakeBetHandlerTest.php
+++ tests/unit/Handler/MakeBetHandlerTest.php
@@ -30,6 +30,18 @@ class MakeBetHandlerTest extends TestCase
         $this->em = $this->prophesize(EntityManagerInterface::class);
         $this->registry->getManager()->willReturn($this->em->reveal())->shouldBeCalled();
         $this->handler = new MakeBetHandler($this->registry->reveal());
+
+        $tester = $this;
+        $this->em->merge(Argument::type(Player::class))->shouldBeCalled()->will(
+            function ($args) use ($tester) {
+                $player = $args[0];
+                $tester->assertCount(1, $player->getBets());
+            }
+        );
+        $this->em->persist(Argument::type(Bet::class))->shouldBeCalled();
+        $this->em->persist(Argument::type(BetSelection::class))->shouldBeCalledTimes(2);
+        $this->em->flush()->shouldBeCalled();
+
     }
 
 
@@ -44,23 +56,20 @@ class MakeBetHandlerTest extends TestCase
             }
         );
 
-        $this->em->merge(Argument::type(Player::class))->shouldBeCalled()->will(
-            function ($args) use ($tester) {
-                $player = $args[0];
-                $tester->assertCount(1, $player->getBets());
-            }
-        );
-        $this->em->persist(Argument::type(Bet::class))->shouldBeCalled()->will(
-            function ($args) use ($tester) {
-                $bet = $args[0];
-                $tester->assertCount(2, $bet->getBetSelections());
-            }
-        );
-
-        $this->em->persist(Argument::type(BetSelection::class))->shouldBeCalledTimes(2);
-        $this->em->flush()->shouldBeCalled();
-
         $message = new MakeBetMessage(1, 100.0, [['id' => 1, 'odds' => '1.623'], ['id' => 2, 'odds' => '1.623']]);
-        $this->handler->__invoke($message);
+        $result = $this->handler->__invoke($message);
+        $this->assertInstanceOf(Bet::class, $result);
+        $this->assertCount(2, $result->getBetSelections());
+    }
+
+    public function testInvoke(): void
+    {
+        $player = new Player();
+        $this->em->find(Player::class, 1)->shouldBeCalled()->willReturn($player);
+        $this->em->persist(Argument::type(Player::class))->shouldNotBeCalled();
+        $message = new MakeBetMessage(1, 100.0, [['id' => 1, 'odds' => '1.623'], ['id' => 2, 'odds' => '1.623']]);
+        $result = $this->handler->__invoke($message);
+        $this->assertInstanceOf(Bet::class, $result);
+        $this->assertSame($player, $result->getPlayer());
     }
 }
